d13a885d23877d731c51fceb904d88d1cffe5b23
Vladislav117 <siluetkysta@gmail.com>
2024-04-01 14:28:03 +0300

    Test balance display

--- app/src/main/java/ru/teambruh/cashguardian/ui/fragments/BalancesFragment.java
+++ app/src/main/java/ru/teambruh/cashguardian/ui/fragments/BalancesFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import ru.teambruh.cashguardian.core.model.Balance;
+import ru.teambruh.cashguardian.core.model.Currency;
 import ru.teambruh.cashguardian.databinding.BalancesFragmentBinding;
 
 public class BalancesFragment extends Fragment {
@@ -15,6 +17,9 @@ public class BalancesFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = BalancesFragmentBinding.inflate(inflater, container, false);
+        for (int i = 10; i < 20; i++) {
+            binding.balancesContainer.addView(new Balance("", "Тестовый баланс #" + i, new Currency("", "₽", 0)).display(getContext()));
+        }
         return binding.getRoot();
     }
 
